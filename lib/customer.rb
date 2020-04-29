3e5dd49719fd943cfac05ad1513836c719de5386
Devone Austin <devone.austin@gmail.com>
2020-04-29 02:38:01 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -14,11 +14,11 @@ class Customer
   end
 
   def new_meal(waiter, total, tip)
-    self.new(waiter, total, tip)
+    Meal.new(waiter, total, tip)
   end
 
   def meals
-    self.meals
+    
   end
 
   def waiters
